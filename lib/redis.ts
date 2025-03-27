1ba314e4b2bfc4ebfebe2d9860ea730abab62570
Sammy2266 <sammymuchai44@gmail.com>
2025-03-27 19:10:28 +0300

    Initial Commit

--- lib/redis.ts
+++ lib/redis.ts
@@ -1,13 +1,4 @@
-import { Redis } from '@upstash/redis';
-import { NextResponse } from 'next/server';
+import { Redis } from "@upstash/redis";
 
 // Initialize Redis
-const redis = Redis.fromEnv();
-
-export const POST = async () => {
-  // Fetch data from Redis
-  const result = await redis.get("item");
-  
-  // Return the result in the response
-  return new NextResponse(JSON.stringify({ result }), { status: 200 });
-};
+export const redis = Redis.fromEnv();
